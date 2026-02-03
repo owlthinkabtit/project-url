13e3e2e6c5529f43af7d9a11ea29eea8bb1b04d1
owlthinkabtit <haggins.jameka@gmail.com>
2026-02-03 12:00:30 -0500

    got the javascript working after so many errors being thrown at me. My main issue, I forget what I name things, change up the naming of things, and don't apply them right away. So it's mostly just me not double checking as I go...d'oh

--- script.js
+++ script.js
@@ -1,21 +1,61 @@
-const button = document.querySelector('.shorten-form button');
-const input = document.querySelector('.shorten-form input');
-const resultsArea = document.querySelector('#shorten-area .container');
+const API_KEY = 'spoo_TQmYef3zgu4p6jd3GxtDIfJk5qIUOVfR1SH1DJnRNQU';
 
-button.addEventListener('click', (event) => {
+const shortenBtn = document.querySelector('.shorten-form button');
+const urlInput = document.querySelector('.shorten-form input');
+// const resultsArea = document.querySelector('#shorten-area .container');
+
+shortenBtn.addEventListener('click', function (event) {
   event.preventDefault();
-  const longUrl = input.ariaValueMax;
 
+  const urlToShorten = urlInput.value;
+
+  if (urlToShorten === "") {
+    alert("Please enter a link first!");
+    return;
+  }
+  console.log("button clicked! We are shortening:", urlToShorten);
+
+  shortenLink(urlToShorten);
+});
+
+function shortenLink(longUrl) {
   fetch('https://spoo.me/', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json' },
-    body: new URLSearchParams({ url: longUrl })
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'Accept': 'application/json',
+      'X-Spoo-Key': 'spoo_TQmYef3zgu4p6jd3GxtDIfJk5qIUOVfR1SH1DJnRNQU'
+    },
+    body: new URLSearchParams({
+      'url': longUrl
+    })
   })
   .then(response => response.json())
   .then(data => {
-    const newLinkBox = document.createElement('div');
-    newLinkBox.className = 'link-result';
-    newLinkBox.innerHTML = `<p>${data.short_url}</p>`;
-    resultsArea.appendChild(newLinkBox);
+   displayLink(longUrl, data.short_url);
+  })
+  .catch(error => {
+    console.error("Oops, something went wrong:", error);
   });
-});
+}
+
+function displayLink(longUrl, shortUrl) {
+  const container = document.getElementById('link-results-container');
+
+  const card = document.createElement('div');
+  card.className = 'link-card';
+
+  card.innerHTML = `
+    <span class="long-url">${longUrl}</span>
+    <span class="short-url">${shortUrl}</span>
+    <button class="btn-copy">Copy</button>
+  `
+  const copyBtn = card.querySelector('.btn-copy');
+  copyBtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(shortUrl);
+    copyBtn.innerText = "Copied!";
+    copyBtn.classList.add('copied');
+  });
+
+  container.prepend(card);
+}
